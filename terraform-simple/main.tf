0a9780813ed51ada986960e56caa9578b0d79216
wwalpha <wwalpha@gmail.com>
2022-11-09 05:10:22 +0000

    update

--- terraform-simple/main.tf
+++ terraform-simple/main.tf
@@ -16,7 +16,11 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 
   use_msi = true
 }
--- terraform-simple/networking/dms.tf
+++ terraform-simple/networking/dms.tf
@@ -5,3 +5,12 @@ resource "azurerm_database_migration_service" "this" {
   subnet_id           = azurerm_subnet.subnet1.id
   sku_name            = "Standard_1vCores"
 }
+
+resource "azurerm_database_migration_project" "this" {
+  name                = "sql_to_sqldb"
+  service_name        = azurerm_database_migration_service.this.name
+  resource_group_name = var.resource_group_name
+  location            = var.resource_group_location
+  source_platform     = "SQL"
+  target_platform     = "SQLDB"
+}
