49fb776470fe2fdd7cf95aeaa551f32219e9fca7
wwalpha <wwalpha@gmail.com>
2022-11-09 11:26:03 +0000

    update

--- terraform-simple/computing/outputs.tf
+++ terraform-simple/computing/outputs.tf
@@ -1,7 +1,3 @@
 output "azurerm_win2012" {
   value = azurerm_windows_virtual_machine.win2012
 }
-
-output "azurerm_win2019" {
-  value = azurerm_windows_virtual_machine.win2019
-}
--- terraform-simple/computing/security_group.tf
+++ terraform-simple/computing/security_group.tf
@@ -4,24 +4,19 @@ resource "azurerm_network_security_group" "win2012" {
   resource_group_name = var.resource_group_name
 }
 
-resource "azurerm_network_interface_security_group_association" "win2012" {
-  depends_on = [
-    azurerm_network_security_group.win2012
-  ]
-  network_interface_id      = azurerm_network_interface.win2012.id
-  network_security_group_id = azurerm_network_security_group.win2012.id
-}
-
-resource "azurerm_network_security_group" "win2019" {
-  name                = "win2019-nsg"
+resource "azurerm_network_interface" "win2012" {
+  name                = "win2012-nic"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.vnet_subnets[0]
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 
-resource "azurerm_network_interface_security_group_association" "win2019" {
-  depends_on = [
-    azurerm_network_security_group.win2019
-  ]
-  network_interface_id      = azurerm_network_interface.win2019.id
-  network_security_group_id = azurerm_network_security_group.win2019.id
+resource "azurerm_network_interface_security_group_association" "win2012" {
+  network_interface_id      = azurerm_network_interface.win2012.id
+  network_security_group_id = azurerm_network_security_group.win2012.id
 }
